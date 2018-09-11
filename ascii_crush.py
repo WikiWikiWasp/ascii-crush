f75bca1616cb077dce7145a5ed261efeea1b7e40
jxf2810 <jason_flinn@homedepot.com>
2018-09-11 11:53:42 -0500

    initial commit

---
+++ ascii_crush.py
@@ -0,0 +1,27 @@
+########################################
+# ASCII CRUSH
+# Author: Jason Flinn
+# Date: September 11th 2018
+########################################
+
+def menu():
+    pass
+
+def draw_board():
+    pass
+
+
+def fill_board():
+    pass
+
+
+def match_chars():
+    pass
+
+
+def remove_match():
+    pass
+
+
+if __name__ == '__main__':
+    menu()
